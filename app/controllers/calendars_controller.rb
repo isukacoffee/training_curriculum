8f165fec92488b4c2485492e765fbc15075ceed5
isukacoffee <isukacoffee4120@gmail.com>
2022-09-24 20:48:39 +0900

    修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
 
   def plan_params
 
-    params.require(:plan).permit(:date,:plan,)
+    params.require(:plan).permit(:date,:plan)
 
   end
 
@@ -40,7 +40,6 @@ class CalendarsController < ApplicationController
       end
 
 
-      require "date"
       wday_num = Date.today.wday + x
       if wday_num >= 7#「wday_numが7以上の場合」という条件式
          wday_num = wday_num -7 
@@ -48,7 +47,7 @@ class CalendarsController < ApplicationController
 
 
 
-      days = {month: (@todays_date + x).month,date: (@todays_date+x).day,plans: today_plans}
+      days = {month: (@todays_date + x).month,date: (@todays_date+x).day,plans: today_plans, wday: wdays[wday_num]}
 
       @week_days.push(days)
     end
